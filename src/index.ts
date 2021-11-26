ec2918c33ca8afbac17700c07c112a8465af207e
deok <39942335+ziinxed@users.noreply.github.com>
2021-11-26 11:59:14 +0900

    Beta fin.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,50 @@
-import { Connection, Context, KeyedAccountInfo } from "@solana/web3.js";
-import { TOKEN_PROGRAM_ID, AccountLayout, u64 } from "@solana/spl-token";
-import base58 from "bs58";
+import {
+  Connection,
+  Context,
+  KeyedAccountInfo,
+  PublicKey,
+} from "@solana/web3.js";
+import { TOKEN_PROGRAM_ID, AccountLayout } from "@solana/spl-token";
+import { publicKey, struct, u64, str } from "@project-serum/borsh";
 import * as amqp from "amqplib";
+import { amountDecoder, publickeyDecoder } from "./utils/decode";
+import { serialize, BinaryReader, BinaryWriter } from "borsh";
+import base58 from "bs58";
+
 require("dotenv").config();
+
+class NewMessage {
+  owner;
+  amount;
+  slot;
+  constructor(data: { owner: string; amount: string; slot: number }) {
+    this.owner = data.owner;
+    this.amount = data.amount;
+    this.slot = data.slot;
+  }
+}
+
+const MSG_SCHEMA = new Map([
+  [
+    NewMessage,
+    {
+      kind: "struct",
+      fields: [
+        ["owner", "string"],
+        ["amount", "string"],
+        ["slot", "u32"],
+      ],
+    },
+  ],
+]);
 
 const main = async () => {
   const cluster = process.env.CLUSTER_ENDPOINT || "localhost:8899";
   const mintAccount = process.env.MINT_ACCOUNT || "";
   const mqEndpoint = process.env.MQ_ENDPOINT || "amqp://127.0.0.1";
   const connection = new Connection(cluster);
+
+  console.log(mqEndpoint);
 
   const accountFilter = [
     { dataSize: AccountLayout.span },
@@ -25,15 +61,21 @@ const main = async () => {
 
   channel.assertExchange(exchange, "direct", { durable: true });
   const sendMessage = (accountInfo: KeyedAccountInfo, ctx: Context): void => {
-    const decodedData = AccountLayout.decode(accountInfo.accountInfo.data);
-    const messageBufferArray = [
-      decodedData.owner,
-      decodedData.amount,
-      Buffer.from(`${ctx.slot}`),
-    ];
-    const message = Buffer.concat(messageBufferArray);
-    channel.publish(exchange, mintAccount, message);
+    const decoded = AccountLayout.decode(accountInfo.accountInfo.data);
+    const data = {
+      owner: publickeyDecoder(decoded.owner),
+      amount: amountDecoder(decoded.amount),
+      slot: ctx.slot,
+    };
+    console.log(data);
+
+    const msg = Buffer.from(serialize(MSG_SCHEMA, new NewMessage(data)));
+
+    channel.publish(exchange, mintAccount, msg);
   };
+
+  console.log(`connected to ${mqEndpoint}`);
+  console.log(`Subscribe to mint account ${mintAccount}`);
 
   const subscriptionId = connection.onProgramAccountChange(
     TOKEN_PROGRAM_ID,
